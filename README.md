648481f56636bff106d41db241ada4f540f4cd6f
piekarowa <pau.piekarska@gmail.com>
2022-10-19 07:03:13 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## cook-book
+
+Web recipe storage application. Useful for small food service businesses where employees can share suggestions for new recipes and other employees can comment them.
+
+## Tech stack
+
+- [Spring Boot](https://spring.io/projects/spring-boot)
+- [Thymeleaf](https://www.thymeleaf.org/)
+- [Bootstrap_CSS](https://yegor256.github.io/tacit/)
+- [MySQL](https://www.mysql.com/)
+
+
+
+## Roadmap
+
+- ☑️ Simple README
+- ⬜ Present results
+- ⬜ add Categories
+- ⬜ add Comments
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,8 @@
-insert into recipe (id, recipe_name, description, ingredients, directions, tag, release_date) values (1, 'sernik', 'z malinami', 'twaróg, jaja, masło, maliny, cukier', 'wymieszać, upiec', 'ciasto', '2022-12-23')
+insert into recipe (id, recipe_name, description, ingredients, directions, tag, release_date) values (1, 'sernik', 'z malinami', 'twaróg, jaja, masło, maliny, cukier', 'wymieszać, upiec', 'ciasto', '2021-12-23');
+insert into recipe (id, recipe_name, description, ingredients, directions, tag, release_date) values (2, 'zupa ogórkowa', 'zupa z kiszonych ogórków', 'ogórki kiszone, bulion, masło', 'gotować na wolnym ogniu', 'zupa', '2021-10-23');
+insert into recipe (id, recipe_name, description, ingredients, directions, tag, release_date) values (3, 'bułki', 'pszenne z otrębami', 'mąka, mleko, drożdże, cukier, sól', 'wymieszać, upiec', 'pieczywo', '2021-12-10');
+insert into recipe (id, recipe_name, description, ingredients, directions, tag, release_date) values (4, 'jabłecznik', 'z kruszonką', 'mąka, jabłka, masło, cukier', 'wymieszać, upiec', 'ciasto', '2021-12-13');
+insert into recipe (id, recipe_name, description, ingredients, directions, tag, release_date) values (5, 'sernik', 'z karmelem', 'twaróg, jaja, masło, karmel, cukier', 'wymieszać, upiec', 'ciasto', '2021-12-23');
 
 insert into role (id, name) values(1, 'ADMIN');
 insert into role (id, name) values(2, 'USER');
